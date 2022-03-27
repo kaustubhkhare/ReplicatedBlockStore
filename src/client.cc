03b2cbc19774b4bc092646f012f6f259a41005c0
Kaustubh Khare <kkhare@node-1.kkhare-120825.advosuwmadison.emulab.net>
2022-03-27 13:01:43 -0600

    grpc setup

---
+++ src/client.cc
@@ -0,0 +1,20 @@
+#include "ds.grpc.pb.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <signal.h>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+#include <fstream>
+#include <memory>
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientWriter;
+
+int main(int argc, char *argv[])
+{
+}
