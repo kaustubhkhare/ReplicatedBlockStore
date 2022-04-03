f7352c156d5426cd3b555e4ec09d52b82edf4343
Kaustubh Khare <kharekaustubh10@gmail.com>
2022-04-03 15:16:48 -0500

    Health client command line args

--- src/health_client.cc
+++ src/health_client.cc
@@ -173,19 +173,19 @@ public:
 };
 
 int main(int argc, char *argv[]) {
-//    if (argc < 5) {
-//        printf("Usage : ./health_client -ip <ip> -port <port>\n");
-//        return 0;
-//    }
+    if (argc < 5) {
+        printf("Usage : ./health_client -ip <ip> -port <port>\n");
+        return 0;
+    }
 
-    std::string ip{"0.0.0.0"}, port{argv[1]};
-//    for (int i = 1; i < argc - 1; ++i) {
-//        if (!strcmp(argv[i], "-ip")) {
-//            ip = std::string{argv[i+1]};
-//        } else if(!strcmp(argv[i], "-port")) {
-//            port = std::string{argv[i+1]};
-//        }
-//    }
+    std::string ip{"0.0.0.0"}, port{"60051"};
+    for (int i = 1; i < argc - 1; ++i) {
+        if (!strcmp(argv[i], "-ip")) {
+            ip = std::string{argv[i+1]};
+        } else if(!strcmp(argv[i], "-port")) {
+            port = std::string{argv[i+1]};
+        }
+    }
     std::string server_address(ip + ":" + port);
     std::vector<std::string> targets {"0.0.0.0:" + std::to_string(constants::PRIMARY_PORT),
                                       "0.0.0.0:" + std::to_string(constants::BACKUP_PORT)};
