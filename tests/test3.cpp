263c6d1917cbb870bad2d49577b919b0eb2be741
prabhav <prabhavad@gmail.com>
2022-04-05 17:24:36 -0500

    fixes in test

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -14,7 +14,7 @@ std::string get_string_with_length(int length, int iteration=1) {
 }
 
 int test3(int argc, char *argv[]) {
-    auto client = *GRPCClient::get_client(argc, argv);
+    auto client = GRPCClient::get_client(argc, argv);
     //read/write aligned reads/writes
     int iteration = 0;
     int length = 100, address_size = 100;
@@ -29,9 +29,9 @@ int test3(int argc, char *argv[]) {
         int offset = addresses[iteration % address_size];
         std::string v = get_string_with_length(length, iteration++);
         std::cout << "Write @ " << offset << '\n';
-        client.write(offset, v.size(), v.c_str());
+        client->write(offset, v.size(), v.c_str());
         std::cout << "Read @ " << offset << '\n';
-        std::string read_str = client.read(offset, length);
+        std::string read_str = client->read(offset, length);
         ASS(!v.compare(read_str), std::string("WRITE AND READ NOT SAME:") +
                                  v +
                                  std::string(",") +
