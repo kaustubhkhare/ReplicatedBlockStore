d4f4979fdb565a9a2e4bec166ac745dd2a636fcb
prabhav <prabhavad@gmail.com>
2022-04-05 17:46:26 -0500

    fixes in test

--- tests/test4.cpp
+++ tests/test4.cpp
@@ -8,7 +8,7 @@
 
 std::string get_string_with_length(int length, int iteration=1) {
     std::string a = std::to_string(iteration);
-    a.append(length - a.size(), iteration);
+    a.append(length - a.size(), '0' + (rand() % 10));
     return a;
 }
 
