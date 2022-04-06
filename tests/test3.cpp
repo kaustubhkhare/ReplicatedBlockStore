28e05f842ac4a048a8c2f4f4e46db3d81021ab8f
prabhav <prabhavad@gmail.com>
2022-04-05 23:28:02 -0500

    fix test

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -49,9 +49,11 @@ int test3(int argc, char *argv[]) {
         int offset = addresses[iteration % address_size];
         std::string v = get_string_with_length_3(length, iteration++);
         std::cout << "Write @ " << offset << "value:" << v << '\n';
-        client->write(offset, v.size(), v.c_str());
-        std::cout << "Read @ " << offset << '\n';
-        std::string read_str = client->read(offset, length);
+        auto status = client->write(offset, v.size(), v.c_str());
+        if (status.ok()) {
+            std::cout << "Read @ " << offset << '\n';
+            std::string read_str = client->read(offset, length);
+        }
     }
 
     return 0;
