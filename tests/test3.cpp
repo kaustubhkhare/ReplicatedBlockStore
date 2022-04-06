7362fed071b96b5659e4ed6095a0cdadd03b1b89
prabhav <prabhavad@gmail.com>
2022-04-05 23:30:25 -0500

    fix test

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -50,7 +50,7 @@ int test3(int argc, char *argv[]) {
         std::string v = get_string_with_length_3(length, iteration++);
         std::cout << "Write @ " << offset << "value:" << v << '\n';
         auto status = client->write(offset, v.size(), v.c_str());
-        if (status.ok()) {
+        if (status != ENONET) {
             std::cout << "Read @ " << offset << '\n';
             std::string read_str = client->read(offset, length);
         }
